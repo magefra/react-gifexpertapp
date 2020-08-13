72a6f0699dddb64f82d552ae9003bc2f90d5e4bc
Magdiel Palacios <magefra9@hotmail.com>
2020-08-13 11:16:48 -0500

    Agregando docs

---
+++ docs/precache-manifest.ec0afc0324db92f57fa0fd7e3e4a3778.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805b324c8f9bff3a639d851931f8054e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4c2a1a0fbc899e9610",
+    "url": "/static/css/main.dfeccede.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b4c2a1a0fbc899e9610",
+    "url": "/static/js/main.a0fbffd0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
